603da76df1cdb24927cd71319b64ceb0407f29f3
go2collage <91822422+go2collage@users.noreply.github.com>
2023-02-21 02:30:01 +0530

    Add files via upload

---
+++ BP_P1.py
@@ -0,0 +1,8 @@
+# Basic Python Program
+
+# US $ => Indian RS.
+
+dollars = float(input("Enter US dollars ($): "))
+
+rupees = dollars * 73.95    # today rate of dollar
+print("US dollars ($) to Indian Rupees: ", rupees)
